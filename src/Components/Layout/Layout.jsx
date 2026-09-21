e6e0785a01c0d1d33de2efb315bfa1e8e677fbad
Aman Kumar <amankumar821017@gmail.com>
2026-09-21 19:36:20 +0530

    Fix scroll position on route change

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
+import ScrollToTop from '../ScrollToTop'
 
 const Layout = () => {
   return (
     
    <div className='sticky -bg-conic-0 top-0'>
+   <ScrollToTop />
   <Navbar />
   <Outlet />
   <Footer />
